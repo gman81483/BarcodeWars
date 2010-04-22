f57645c894d9a010b17ec0dfbdf05094c9ea6e99
Steve Aquillano <steve@Lisa-Aquillanos-MacBook-Pro.local>
2010-04-22 00:11:58 -0400

    added BarcodeWarsNoScan

---
+++ BarcodeWarsNoScan/src/com/dev/BarcodeWarsNoScan/BarcodeWarsNoScan.java
@@ -0,0 +1,140 @@
+package com.dev.BarcodeWarsNoScan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dev.BarcodeWarsNoScan.R;
+import com.dev.BarcodeWarsNoScan.R.id;
+import com.dev.BarcodeWarsNoScan.R.layout;
+import android.app.Activity; 
+import android.content.*;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View; 
+import android.widget.*;
+
+/**Class BarcodeWars is the driver class behind the BarcodeWars application
+ * 
+ * @author Greg Charette
+ * @author Steve Aquillano
+ *
+ */
+public class BarcodeWarsNoScan extends Activity 
+{ 
+	Button scanButton;
+	TextView scanResultText;
+	int wins = 0;
+	int losses = 0;
+	int energy = 0;
+	int infantry = 0;
+	int knowledge = 0;
+	String increasedAttribute = null;
+	DataStoreTextNoScan dataStore;
+	
+	/**onCreate instantiates the initial program objects and sets up layout when program
+	 * is opened
+	*/
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{ 
+		super.onCreate(savedInstanceState); 
+		setContentView(R.layout.main);
+
+		scanButton = (Button) this.findViewById(R.id.scanButton);
+		findViewById(R.id.scanButton).setOnClickListener(ocl);
+		scanResultText = (TextView) this.findViewById(R.id.scanResultText);
+		
+		Context appContext = getApplicationContext();
+		dataStore = new DataStoreTextNoScan(appContext);
+	}
+	
+	/**Method listens for the scanning button to be clicked.  When this happens,
+	 * a new intent is created that opens the scanning class and calls @startActivityForResult
+	 * to start performing the desired operation
+	*/
+	public final Button.OnClickListener ocl = new Button.OnClickListener() {
+	    public void onClick(View v) {
+	    	
+	    	String scanResult = calcScanPoints("A19N25L567");
+	    	scanResultText.setText("Excellent! This scan increased your " + increasedAttribute + " by " + scanResult + "!\n\nYour current stats are:\nEnergy: " + energy + "\nInfantry: " + infantry + "\nKnowledge: " + knowledge);
+	    }
+	  };
+
+	/**onActivityResult initiates scanner activity, passing the barcode into the method, which calls
+	 * the method @calcScanPoints to calculate the total points the player receives from
+	 * the object that was scanned
+	*/
+	/*public void onActivityResult(int requestCode, int resultCode, Intent intent) 
+	{
+	    if (requestCode == 0) 
+	    {
+	    	if (resultCode == RESULT_OK) 
+	    	{
+	    		
+	    		//String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	    		scanResultText.setText("Excellent! This scan increased your " + increasedAttribute + " by " + scanResult + "!\n\nYour current stats are:\nEnergy: " + energy + "\nInfantry: " + infantry + "\nKnowledge: " + knowledge);
+
+	    		/*if(format.equals("ISBN"))
+	    		{
+	    			knowledge += Integer.parseInt(scanResult);
+	    		}
+	    	} 
+	    	
+	    	else if (resultCode == RESULT_CANCELED) 
+	    	{
+	    		//showDialog(R.string.result_failed, getString(R.string.result_failed_why));
+	    	}
+	    }
+	}*/
+	/**calcScanPoints takes in the result of the barcode scan, calculates the total number of points for the scan,
+	 * and assigns the points to the appropriate player attribute
+	 * @param scanResult
+	 * @return - String calcScanPoints - Total points gained from the object scanned
+	 * @throws NullPointerException
+	 */
+	public String calcScanPoints(String scanResult) throws NullPointerException
+	{
+		int finalScannedPoints = 0;
+		Map<String, String> hm = new HashMap<String, String>();
+		
+		String[] keys = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		String[] values = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36"};
+		
+		for(int i = 0; i < keys.length; i++)
+		{
+			hm.put(keys[i], values[i]);
+		}
+		
+		for(int i = 0; i < scanResult.length(); i++)
+		{
+			if(!scanResult.equals(null))
+			{	
+				finalScannedPoints += Integer.parseInt((String)hm.get(scanResult.substring(i,i+1).toUpperCase()));
+				
+			}
+		}
+		
+		int randomNum = (int)Math.round(Math.random() * 2);
+		
+		switch(randomNum)
+		{
+			case 0: energy += finalScannedPoints;
+				increasedAttribute = "Energy";
+				dataStore.setEnergy(energy);
+				break;
+			case 1: infantry += finalScannedPoints;
+				increasedAttribute = "Infantry";
+				dataStore.setInfantry(infantry);
+				break;
+			case 2: knowledge += finalScannedPoints;
+				increasedAttribute = "Knowledge";
+				dataStore.setKnowledge(knowledge);
+				break;
+		}
+		
+		Log.v("randomNumber: ", Integer.toString(randomNum));
+		
+		return Integer.toString(finalScannedPoints);
+	} 
+}
+	
---
+++ BarcodeWarsNoScan/src/com/dev/BarcodeWarsNoScan/DataStoreTextNoScan.java
@@ -0,0 +1,484 @@
+/**
+ * 
+ */
+package com.dev.BarcodeWarsNoScan;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+/**
+ * @author Greg Charette
+ * @author Steve Aquillano
+ *
+ */
+public class DataStoreTextNoScan {
+	
+	private int wins = 0;
+	private int losses = 0;
+	private int energy = 0;
+	private int infantry = 0;
+	private int knowledge = 0;
+	BufferedReader in;
+	private PrintWriter out;
+	Context appContext;
+	
+	Vector<Integer> vect = new Vector<Integer>();
+	
+	public DataStoreTextNoScan(Context context)
+	{
+		Log.v("Entering DataStoreText Constructor", "YEAH BABY");
+		initAttributes();
+		
+		appContext = context;
+	}
+	
+	private void initAttributes()
+	{
+		Log.v("In INITATTRIBUTES", "HELLZ YEAH!!!!");
+		//String stores information read from file
+		String line;
+		
+		//Try allows for error catching
+		try
+		{	
+			//Opens a file to read
+			FileInputStream fis = appContext.openFileInput("datastore");
+			
+			in = new BufferedReader(new InputStreamReader(fis));
+			
+			//Runs while there is still information to read
+			while((line = in.readLine()) != null)
+			{		
+				//If statement runs if employee is a salaried employee
+				if(line.substring(0,3).equals("wins"))
+				{
+					//Try allows for error catching
+					try
+					{
+						wins = Integer.parseInt(line.substring(5,line.length() - 1));
+					}
+						
+					//Catches BadAttributeValueException and lets user know
+					catch(Exception e)
+					{
+						System.out.print("\n" + e);
+					}
+				}
+				
+				//If statement runs if employee is a salaried employee
+				if(line.substring(0,5).equals("losses"))
+				{
+					//Try allows for error catching
+					try
+					{
+						losses = Integer.parseInt(line.substring(7,line.length() - 1));
+					}
+						
+					//Catches BadAttributeValueException and lets user know
+					catch(Exception e)
+					{
+						System.out.print("\n" + e);
+					}
+				}
+				
+				//If statement runs if employee is a salaried employee
+				if(line.substring(0,5).equals("energy"))
+				{
+					//Try allows for error catching
+					try
+					{
+						energy = Integer.parseInt(line.substring(7,line.length() - 1));
+					}
+						
+					//Catches BadAttributeValueException and lets user know
+					catch(Exception e)
+					{
+						System.out.print("\n" + e);
+					}
+				}
+				
+				//If statement runs if employee is a salaried employee
+				if(line.substring(0,7).equals("infantry"))
+				{
+					//Try allows for error catching
+					try
+					{
+						infantry = Integer.parseInt(line.substring(9,line.length() - 1));
+					}
+						
+					//Catches BadAttributeValueException and lets user know
+					catch(Exception e)
+					{
+						System.out.print("\n" + e);
+					}
+				}
+				
+				if(line.substring(0,8).equals("knowledge"))
+				{
+					//Try allows for error catching
+					try
+					{
+						knowledge = Integer.parseInt(line.substring(10,line.length() - 1));
+					}
+						
+					//Catches BadAttributeValueException and lets user know
+					catch(Exception e)
+					{
+						System.out.print("\n" + e);
+					}
+				}
+				
+				try
+				{
+					in.close();
+				}
+				
+				catch(Exception e)
+				{
+					System.out.print("\n" + e);
+				}
+			}
+		}
+		
+		//Catches IO Exception and lets user know
+		catch(Exception e)
+		{
+			System.out.print("\n" + e);
+		}
+		
+		vect.add(wins);
+		vect.add(losses);
+		vect.add(energy);
+		vect.add(infantry);
+		vect.add(knowledge);
+	}
+	
+	public int getWins()
+	{
+		return wins;
+	}
+	
+	public int getLosses()
+	{
+		return losses;
+	}
+	
+	public int getEnergy()
+	{
+		return energy;
+	}
+	
+	public int getInfantry()
+	{
+		return infantry;
+	}
+	
+	public int getKnowledge()
+	{
+		return knowledge;
+	}
+	
+	public void setWins(int winsIncrease)
+	{
+		wins += winsIncrease;
+		vect.setElementAt(wins, 0);
+		int count = 0;
+		
+		try
+		{	
+			FileOutputStream fos = appContext.openFileOutput("datastore", 0);
+			out = new PrintWriter(new OutputStreamWriter(fos));
+		
+			Iterator<Integer> iter = vect.iterator();
+			
+			while(iter.hasNext())
+			{
+				if(count == 0)
+				{
+					//out.println("wins=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 1)
+				{
+					//out.println("losses=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 2)
+				{
+					//out.println("energy=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 3)
+				{
+					//out.println("infantry=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 4)
+				{
+					//out.println("knowledge=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				count++;
+			}
+			
+			out.flush();
+			
+			out.close();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("\n" + e);
+		}
+	}
+	
+	public void setLosses(int lossesIncrease)
+	{
+		losses += lossesIncrease;
+		vect.setElementAt(losses, 1);
+		int count = 0;
+		
+		try
+		{
+			FileOutputStream fos = appContext.openFileOutput("datastore", 0);
+			out = new PrintWriter(new OutputStreamWriter(fos));
+		
+			Iterator<Integer> iter = vect.iterator();
+			
+			while(iter.hasNext())
+			{
+				if(count == 0)
+				{
+					//out.println("wins=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 1)
+				{
+					//out.println("losses=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 2)
+				{
+					//out.println("energy=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 3)
+				{
+					//out.println("infantry=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 4)
+				{
+					//out.println("knowledge=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				count++;
+			}
+			
+			out.flush();
+			
+			out.close();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("\n" + e);
+		}
+	}
+	
+	public void setEnergy(int energyIncrease)
+	{
+		energy += energyIncrease;
+		vect.setElementAt(energy, 2);
+		int count = 0;
+		
+		try
+		{
+			FileOutputStream fos = appContext.openFileOutput("datastore", 0);
+			out = new PrintWriter(new OutputStreamWriter(fos));
+		
+			Iterator<Integer> iter = vect.iterator();
+			
+			while(iter.hasNext())
+			{
+				if(count == 0)
+				{
+					//out.println("wins=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 1)
+				{
+					//out.println("losses=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 2)
+				{
+					//out.println("energy=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 3)
+				{
+					//out.println("infantry=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 4)
+				{
+					//out.println("knowledge=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				count++;
+			}
+			
+			out.flush();
+			
+			out.close();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("\n" + e);
+		}
+	}
+	
+	public void setInfantry(int infantryIncrease)
+	{
+		infantry += infantryIncrease;
+		vect.setElementAt(infantry, 3);
+		int count = 0;
+		
+		try
+		{     
+			FileOutputStream fos = appContext.openFileOutput("datastore", 0);
+			out = new PrintWriter(new OutputStreamWriter(fos));
+		
+			Iterator<Integer> iter = vect.iterator();
+			
+			while(iter.hasNext())
+			{
+				if(count == 0)
+				{
+					//out.println("wins=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 1)
+				{
+					//out.println("losses=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 2)
+				{
+					//out.println("energy=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 3)
+				{
+					//out.println("infantry=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 4)
+				{
+					//out.println("knowledge=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				count++;
+			}
+			
+			out.flush();
+			
+			out.close();
+		}
+			
+		catch(Exception e)
+		{
+			System.out.print("\n" + e);
+		}
+	}
+	
+	public void setKnowledge(int knowledgeIncrease)
+	{
+		knowledge += knowledgeIncrease;
+		vect.setElementAt(knowledge, 4);
+		int count = 0;
+		
+		try
+		{
+			FileOutputStream fos = appContext.openFileOutput("datastore", 0);
+			out = new PrintWriter(new OutputStreamWriter(fos));
+		
+			Iterator<Integer> iter = vect.iterator();
+			
+			while(iter.hasNext())
+			{
+				if(count == 0)
+				{
+					//out.println("wins=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 1)
+				{
+					//out.println("losses=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 2)
+				{
+					//out.println("energy=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 3)
+				{
+					//out.println("infantry=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				if(count == 4)
+				{
+					//out.println("knowledge=" + iter.next());
+					Log.v("Iterator value: ", Integer.toString(iter.next()));
+				}
+				
+				count++;
+			}
+			
+			out.flush();
+			
+			out.close();
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("\n" + e);
+		}
+	}
+}
